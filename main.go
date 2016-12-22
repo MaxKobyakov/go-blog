00aa80fda5cc8406111ffab0b9941a9c9f9e7143
MaxKobyakov <kobyakov.m@gmail.com>
2016-12-22 17:59:24 +0300

    stabl

--- main.go
+++ main.go
@@ -2,9 +2,24 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"net/http"
 )
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("templates/index.html")
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+
+	t.ExecuteTemplate(w, "index", nil)
+}
 
 func main() {
 	fmt.Println("Слушаем порт: 3000")
+
+	http.HandleFunc("/", indexHandler)
+
+	http.ListenAndServe(":3000", nil)
 
 }
