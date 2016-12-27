c377b848ae4c5d1bb29595e5b4c3b8f9063cc722
MaxKobyakov <kobyakov.m@gmail.com>
2016-12-27 17:56:01 +0300

    NewPost

--- main.go
+++ main.go
@@ -7,13 +7,17 @@ import (
 	"net/http"
 )
 
+var posts map[string]*models.Post
+
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	t, err := template.ParseFiles("templates/index.html", "templates/header.html", "templates/footer.html")
 	if err != nil {
 		fmt.Fprintf(w, err.Error())
 	}
 
-	t.ExecuteTemplate(w, "index", nil)
+	fmt.Println(posts)
+
+	t.ExecuteTemplate(w, "index", posts)
 }
 
 func writeHandler(w http.ResponseWriter, r *http.Request) {
@@ -29,10 +33,19 @@ func savePostHandler(w http.ResponseWriter, r *http.Request) {
 	id := r.FormValue("id")
 	title := r.FormValue("title")
 	content := r.FormValue("content")
+
+	post := models.NewPost(id, title, content)
+	posts[post.Id] = post
+
+	http.Redirect(w, r, "/", 302)
+
 }
 
 func main() {
 	fmt.Println("Слушаем порт: 3000")
+
+	posts = make(map[string]*models.Post, 0)
+
 	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer((http.Dir("./assets/")))))
 	http.HandleFunc("/", indexHandler)
 	http.HandleFunc("/write", writeHandler)
